82ff64a4bd17123a3b902a9de89c617d22fe964c
Mario Krastev <mario@nexo.io>
2021-09-23 11:12:29 +0300

    test

--- cypress/integration/3-nexoExample/nexo.spec.js
+++ cypress/integration/3-nexoExample/nexo.spec.js
@@ -3,6 +3,8 @@ import registerPage from '../../support/pageObject/register'
 describe('hello google',() => {   
 
     let reg = new registerPage();
+    const testEmail = `testname_nexo${reg.getRandomNumber()}@mailinator.com`
+    const testName = `testname_nexo${reg.getRandomNumber()}`
 
     beforeEach(() => {
         
@@ -14,9 +16,21 @@ describe('hello google',() => {
     it('open new page', () => {
 
         reg.registerButton().click();
-        cy.get('form#accountFrm button[type="submit"]').should('be.visible').click();
-        cy.get('input#AccountFrm_firstname').type('Mario');
-        cy.get('input#AccountFrm_lastname').type('Krastev');
+        reg.continueButton().click();
+        reg.firstNameField().type('Mario');
+        reg.lastNameField().type('Krastev');
+        reg.emailField().type(testEmail);
+        reg.addressField().type('test test test');
+        reg.cityField().type('sofia');
+        reg.regionField().select('Bristol');
+        reg.zipCodeField().type('1220');
+        reg.loginNameField().type(testName);
+        reg.passwordField().type('makaveli');
+        reg.passwordConfirmField().type('makaveli');
+        reg.subscribeField().click();
+        reg.privacyButton().click();
+        reg.continueButtonSecond().click();
+
 
     })
 
--- cypress/support/pageObject/register.js
+++ cypress/support/pageObject/register.js
@@ -1,9 +1,71 @@
 class registerPage {
 
-    registerButton() {
-        return cy.get('ul#customer_menu_top a')
+    registerButton(){
+        return cy.get('ul#customer_menu_top a').should('be.visible')
     }
 
+    continueButton(){
+        return cy.get('form#accountFrm button[type="submit"]').should('be.visible');
+    }
+
+    firstNameField(){
+        return cy.get('input#AccountFrm_firstname').should('be.visible');
+    }
+
+    lastNameField(){
+        return cy.get('input#AccountFrm_lastname').should('be.visible');
+    }
+
+    emailField(){
+        return cy.get('input#AccountFrm_email').should('be.visible');
+    }
+
+    addressField(){
+        return cy.get('input#AccountFrm_address_1').should('be.visible');
+    }
+
+    cityField(){
+        return cy.get('input#AccountFrm_city').should('be.visible');
+    }
+
+    regionField(){
+        return cy.get('select#AccountFrm_zone_id').should('be.visible');
+    }
+
+    zipCodeField(){
+        return cy.get('input#AccountFrm_postcode').should('be.visible');
+    }
+
+    loginNameField(){
+        return cy.get('input#AccountFrm_loginname').should('be.visible');
+    }
+
+    passwordField(){
+        return cy.get('input#AccountFrm_password').should('be.visible');
+    }
+
+    passwordConfirmField(){
+        return cy.get('input#AccountFrm_confirm').should('be.visible');
+    }
+
+    subscribeField(){
+        return cy.get('input#AccountFrm_newsletter0').should('be.visible');
+    }
+
+    privacyButton(){
+        return cy.get('input#AccountFrm_agree').should('be.visible');
+    }
+
+    continueButtonSecond(){
+        return cy.get('form#AccountFrm button[type="submit"]').should('be.visible');
+    }
+
+    getRandomNumber(){
+
+        const uuid = () => Cypress._.random(0, 1e6);
+        return uuid()
+
+    }
 
 
 }
