9d0bc7bdf4b5384bc80f09c031985549fc913740
Mario Krastev <mario@nexo.io>
2021-09-28 16:32:40 +0300

    test

--- cypress/integration/3-nexoExample/nexo.spec.js
+++ cypress/integration/3-nexoExample/nexo.spec.js
@@ -5,6 +5,13 @@ describe('hello google',() => {
     let reg = new registerPage();
     const testEmail = `testname_nexo${reg.getRandomNumber()}@mailinator.com`
     const testName = `testname_nexo${reg.getRandomNumber()}`
+    let firstName = 'Mario',
+    secondName = 'Krastev',
+    address = 'test address',
+    city = 'Nadezhda',
+    region = 'Bristol',
+    zipCode = '1220',
+    password = 'makaveli';
 
     beforeEach(() => {
         
@@ -13,24 +20,24 @@ describe('hello google',() => {
 
     })
 
-    it('open new page', () => {
+    it('new register', () => {
 
         reg.registerButton().click();
         reg.continueButton().click();
-        reg.firstNameField().type('Mario');
-        reg.lastNameField().type('Krastev');
+        reg.firstNameField().type(firstName);
+        reg.lastNameField().type(secondName);
         reg.emailField().type(testEmail);
-        reg.addressField().type('test test test');
-        reg.cityField().type('sofia');
-        reg.regionField().select('Bristol');
-        reg.zipCodeField().type('1220');
+        reg.addressField().type(address);
+        reg.cityField().type(city);
+        reg.regionField().select(region);
+        reg.zipCodeField().type(zipCode);
         reg.loginNameField().type(testName);
-        reg.passwordField().type('makaveli');
-        reg.passwordConfirmField().type('makaveli');
+        reg.passwordField().type(password);
+        reg.passwordConfirmField().type(password);
         reg.subscribeField().click();
         reg.privacyButton().click();
-        reg.continueButtonSecond().click();
-
+        reg.continueButtonSecond().click();             
+    
 
     })
 
